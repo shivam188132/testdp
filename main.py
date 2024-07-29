4328b012d00be8f54756f1ef02e4134dc2415ee2
Shivam <Shivamkumar188132@gmail.com>
2024-07-29 13:48:24 +0530

    added prettytable_practise.py for practise

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from turtle import Turtle
-
+from turtle import Turtle, Screen
+import prettytable
 timmy = Turtle()
 print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+timmy.shape("turtle")
+timmy.color('CornflowerBlue')
+timmy.penup()
+timmy.forward(100)
+
+my_screen.exitonclick()
