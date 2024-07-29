06ed00213fa87617cbc39311826493e03815c19d
Shivam <Shivamkumar188132@gmail.com>
2024-07-29 14:10:06 +0530

    setting allingment

--- prettytable_practise.py
+++ prettytable_practise.py
@@ -1,1 +1,10 @@
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokeymon Name", ["Combee", "Roserade", "Ledyba", "Ledian"])
+table.add_column("Pokeymon Type", ["Flying", "Poison", "Flying", "Ledian"])
+table.align = "l"
+
+print(table)
+
